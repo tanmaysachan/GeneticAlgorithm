474d37da7024c21ede564a290e7a3be71ce702f8
tanmaysachan <tnmysachan@gmail.com>
2020-03-20 02:59:00 +0530

    clean stuff

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ from client_moodle import get_errors, submit
 
 team_secret_key = 'I22KGMKf3ZqtxxvxklykgAlk1dQZvVqhgfZT1i8NWjOgBC4ntl'
 
+
 def cal_pop_fitness(pop):
     fitness = [get_errors(team_secret_key, list(i))[0] for i in pop]
     print(fitness)
@@ -43,19 +44,15 @@ def distort(vector):
 
 num_weights = 11
 
-sol_per_pop = 8
+population = 8
 
 num_parents_mating = 4
 
-pop_size = (sol_per_pop, num_weights)
-
 overfit_model = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
 
-new_population = np.array([distort(overfit_model) for i in range(sol_per_pop)])
+new_population = np.array([distort(overfit_model) for i in range(population)])
 
-print(new_population)
-print(new_population.shape)
-
+# generations to train for
 num_generations = 5
 
 for generation in range(num_generations):
@@ -65,8 +62,7 @@ for generation in range(num_generations):
     parents = select_mating_pool(new_population, fitness,
                                       num_parents_mating)
 
-    offspring_crossover = crossover(parents,
-                                       offspring_size=(pop_size[0]-parents.shape[0], num_weights))
+    offspring_crossover = crossover(parents, (population-parents.shape[0], num_weights))
 
     offspring_mutation = mutation(offspring_crossover)
 
@@ -75,6 +71,7 @@ for generation in range(num_generations):
 
     print("Fitness so far : ")
     print(fitness)
+
 
 fitness = cal_pop_fitness(new_population)
 best_match_idx = np.where(fitness == np.min(fitness))[0][0]
@@ -85,6 +82,6 @@ print("weights_vector")
 print(weights_vector)
 print("Best solution fitness : ", fitness[best_match_idx])
 
-#submit stuff
+# submit stuff
 
 submit(team_secret_key, list(weights_vector))
